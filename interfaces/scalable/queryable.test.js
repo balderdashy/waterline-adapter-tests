d207d8b07c563be023c59bf6fe77301c35d7c56a
Mike McNeil <michael.r.mcneil@gmail.com>
2014-01-08 12:58:37 -0600

    Semantic benchmark work

--- interfaces/scalable/queryable.test.js
+++
@@ -1,9 +0,0 @@
-
-
-// TODO: test functionality for the queryable interface at high concurrency
-
-// TODO: measure memory usage
-// TODO: measure execution time
-// TODO: track any failures/errors
-
-
