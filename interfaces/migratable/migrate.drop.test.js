1f7db88ea4f40ee19a8d0971ced8d9717501d38b
Mike McNeil <michael.r.mcneil@gmail.com>
2014-02-01 20:55:19 -0600

    Use new bootstrapAndDescribe helper.

--- interfaces/migratable/migrate.drop.test.js
+++ interfaces/migratable/migrate.drop.test.js
@@ -1,70 +1,62 @@
 /**
  * Module dependencies
  */
-var bootstrap = require('../../lib/bootstrap'),
+ 
+var bootstrapAndDescribe = require('../../lib/bootstrapAndDescribe'),
   assert = require('assert'),
   should = require('should');
 
 
 
 describe('Migratable Interface', function() {
-
-
   describe('migrate: "drop"', function() {
 
-    var ontology, waterline;
-
     //
-    // Setup waterline
-    //
+    // Side note on using connection objects:
+    // 
+    // eventually, access to connections will be wrapped up:
+    // conn.describe('foo', console.log);
+    // 
 
-    before(function(done) {
-      waterline = bootstrap({
-        adapters: {
-          wl_tests: Adapter
-        },
-        connections: Connections,
-        collections: {
-          pirate: {
-            connection: 'test',
-            attributes: {
-              name: 'string',
-              age: 'integer'
-            }
+    bootstrapAndDescribe({
+      collections: {
+        pirate: {
+          migrate: 'drop',
+          connection: 'test',
+          attributes: {
+            name: 'string',
+            age: 'integer'
           }
         }
-      }, function(err, _ontology) {
-        if(err) return done(err);
-        ontology = _ontology;
-        done();
+      }
+    }, function (ontology) {
+
+      it('sanity check first...', function () {
+        ontology.should.be.an.Object;
+        ontology.collections.should.be.an.Object;
+        ontology.collections.pirate.should.be.an.Object;
+        ontology.collections.pirate.migrate
+          .should.equal('drop');
       });
-    });
 
-    after(function(done) {
-      waterline.teardown(done);
-    });
+      it('should have created tables', function (done) {      
+        var conn = ontology.connections.test;
+        conn._adapter.describe('test', 'pirate', function (err, schema) {
+          should(schema).be.an.Object;
+          done(err);
+        });
+      });
 
-
-
-    //
-    // Test that migration was successful
-    //
-
-    it('should have created tables', function (done) {
-
-      // eventually, access to connections will be wrapped up:
-      // conn.describe('foo', console.log);
-
-      // for now:
-      var conn = ontology.connections.test;
-      conn._adapter.describe('test', 'pirate', function (err, schema) {
-
-        should(schema).be.an.Object;
-
-        done(err);
+      it('should have deleted any data that was there', function (done) {
+        var Pirate = ontology.collections.pirate;
+        Pirate.count().exec(function (err, numPirates) {
+          assert(numPirates === 0);
+          return done(err);
+        });
       });
 
     });
+
 
   });
 
