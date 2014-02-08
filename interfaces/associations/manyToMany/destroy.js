9cce9f3682c8fd2271c2b512edc243ee9bb5b72a
Cody Stoltman <particlebanana@gmail.com>
2014-02-07 18:25:16 -0600

    destroy join table values when a many to many record is destroyed

---
+++ interfaces/associations/manyToMany/destroy.js
@@ -0,0 +1,55 @@
+var assert = require('assert'),
+    _ = require('lodash');
+
+describe('Association Interface', function() {
+
+  describe('n:m association :: .destroy()', function() {
+
+    /////////////////////////////////////////////////////
+    // TEST SETUP
+    ////////////////////////////////////////////////////
+
+    var driverRecord;
+
+    before(function(done) {
+      Associations.Driver.create({ name: 'manymany destroy'}, function(err, driver) {
+        if(err) return done(err);
+
+        driverRecord = driver;
+
+        var taxis = [];
+        for(var i=0; i<2; i++) {
+          driverRecord.taxis.add({ medallion: i });
+        }
+
+        driverRecord.save(function(err) {
+          if(err) return done(err);
+          done();
+        });
+      });
+    });
+
+    /////////////////////////////////////////////////////
+    // TEST METHODS
+    ////////////////////////////////////////////////////
+
+    it('should remove join table records when a parent is destroyed', function(done) {
+
+      var parentId = driverRecord.id;
+
+      Associations.Driver.destroy({ name: 'manymany destroy' })
+      .exec(function(err, status) {
+        if(err) return done(err);
+
+        Associations.Driver_taxis__taxi_drivers.find({ driver_taxis: parentId })
+        .exec(function(err, records) {
+          if(err) return done(err);
+
+          assert(records.length === 0);
+          done();
+        });
+      });
+    });
+
+  });
+});
