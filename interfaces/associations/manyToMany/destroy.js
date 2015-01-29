78b71115255f16baa3fc9212e33d92d271753def
Cody Stoltman <particlebanana@gmail.com>
2015-01-29 13:51:52 -0600

    add test for making sure join table records don't get destroyed when criteria is missing

--- interfaces/associations/manyToMany/destroy.js
+++ interfaces/associations/manyToMany/destroy.js
@@ -52,6 +52,55 @@ describe('Association Interface', function() {
 
   });
 
+  describe('when criteria returns no results', function () {
+
+    /////////////////////////////////////////////////////
+    // TEST SETUP
+    ////////////////////////////////////////////////////
+
+    var driverRecord;
+
+    before(function(done) {
+      Associations.Driver.create({ name: 'manymany destroy'}, function(err, driver) {
+        if(err) return done(err);
+
+        driverRecord = driver;
+
+        var taxis = [];
+        for(var i=0; i<2; i++) {
+          driverRecord.taxis.add({ medallion: i });
+        }
+
+        driverRecord.save(function(err) {
+          if(err) return done(err);
+          done();
+        });
+      });
+    });
+
+    /////////////////////////////////////////////////////
+    // TEST METHODS
+    ////////////////////////////////////////////////////
+
+    it('should not destroy any join table records', function (done) {
+      Associations.Driver.destroy({ name: 'manymany destroy_foo' }, function (err, status) {
+        assert(!err);
+
+        // Find records in the join table
+        Associations.Driver_taxis__taxi_drivers.find({ driver_taxis: driverRecord.id }).exec(function(err, records) {
+          if(err) return done(err);
+          if(!records.length) {
+            return done(new Error('Removed all the join table records!'));
+          }
+
+          return done();
+        });
+      });
+    });
+
+  });
+
+
   describe('destroying an entire collection', function () {
 
     it('should not result in an error', function (done) {
