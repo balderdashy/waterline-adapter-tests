9dd7f62b28e7073f7ce17d63fc3ce37efe47134d
Dario Marcelino <dario@appscot.com>
2015-03-24 21:56:54 +0000

    Integration tests: adds sails-disk, sails-mongo and sails-mysql travis.yml: make sure waterline is the latest from master

--- test/integration/runner.sh
+++ test/integration/runner.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+EXIT_CODE=0
+
+
 function getStatus {
 	if [ $1 -eq 0 ]
 	then
 		STATUS="\033[0;32msuccess\033[0m"
 	else
 		STATUS="\033[0;31mfailed \033[0m"
+		EXIT_CODE=1
 	fi
 }
 
@@ -17,12 +21,29 @@ node test/integration/runner.js sails-memory
 getStatus $?
 MEMORY=$STATUS
 
+node test/integration/runner.js sails-disk
+getStatus $?
+DISKST=$STATUS
+
+node test/integration/runner.js sails-mongo
+getStatus $?
+MONGOD=$STATUS
+
+node test/integration/runner.js sails-mysql
+getStatus $?
+MYSQLD=$STATUS
+
+
 echo
-echo    " -----------------------------------"
-echo    "| adapter          | status         |"
-echo    "|------------------|----------------|"
-echo -e "| sails-postgresql | $POSTGR        |"
-echo -e "| sails-memory     | $MEMORY        |"
-echo    " -----------------------------------"
+echo    " ---------------------------- "
+echo    "| adapter          | status  |"
+echo    "|------------------|---------|"
+echo -e "| sails-postgresql | $POSTGR |"
+echo -e "| sails-memory     | $MEMORY |"
+echo -e "| sails-disk       | $DISKST |"
+echo -e "| sails-mongo      | $MONGOD |"
+echo -e "| sails-mysql      | $MYSQLD |"
+echo    " ---------------------------- "
 
 
+exit $EXIT_CODE
