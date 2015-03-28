cad9414f81d718402f03bfe7bb9c7ddc9e06a731
Dario Marcelino <dario@appscot.com>
2015-03-28 16:10:25 +0000

    #40: added `.query()` tests to interface SQL.

---
+++ interfaces/sql/query.test.js
@@ -0,0 +1,40 @@
+var assert = require('assert'),
+    _ = require('lodash');
+
+describe('SQL Interface', function() {
+
+  describe('.query()', function() {
+
+    /////////////////////////////////////////////////////
+    // TEST SETUP
+    ////////////////////////////////////////////////////
+
+    before(function(done) {
+
+      // Insert 1 User
+      Sql.User.create({first_name: 'query_user', type: 'query test', age: 10 }, function(err, user) {
+        if(err) return done(err);
+        done();
+      });
+    });
+
+    /////////////////////////////////////////////////////
+    // TEST METHODS
+    ////////////////////////////////////////////////////
+    
+    it('should have method .query()', function(done) {
+      assert(Sql.User.query);
+      assert(_.isFunction(Sql.User.query));
+      done();
+    });
+
+    it('should return non undefined result', function(done) {
+      Sql.User.query('SELECT * FROM userTableSql', undefined, function(err, users) {
+        assert(!err);
+        assert(users);
+        done();
+      });
+    });
+
+  });
+});
