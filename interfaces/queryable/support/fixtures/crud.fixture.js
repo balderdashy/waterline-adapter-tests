affe9d204facfabbce5efe5f970269befc4c4f11
Cody Stoltman <particlebanana@gmail.com>
2016-12-09 18:40:06 -0600

    cleanup queryable bootstrap

--- interfaces/queryable/support/fixtures/crud.fixture.js
+++ interfaces/queryable/support/fixtures/crud.fixture.js
@@ -5,34 +5,108 @@
 var Waterline = require('waterline');
 
 module.exports = Waterline.Collection.extend({
-
   tableName: 'userTable2',
   identity: 'user',
-  connection: 'queryable',
+  datastore: 'queryable',
+  primaryKey: 'id',
 
   attributes: {
-    first_name: 'string',
-    last_name: 'string',
-    email: 'string',
-    title: 'string',
-    phone: 'string',
-    type: 'string',
-    favoriteFruit: {
-      defaultsTo: 'blueberry',
-      type: 'string'
+    // Primary Key
+    id: {
+      type: 'number',
+      autoMigrations: {
+        columnType: 'integer',
+        autoIncrement: true,
+        unique: true
+      }
     },
-    age: 'integer', // integer field that's not auto-incrementable
-    dob: 'date',
-    status: {
-      type: 'boolean',
-      defaultsTo: false
+
+    first_name: {
+      type: 'string',
+      autoMigrations: {
+        columnType: 'varchar'
+      }
     },
-    percent: 'float',
-    list: 'array',
-    obj: 'json',
-    fullName: function() {
-      return this.first_name + ' ' + this.last_name;
+
+    last_name: {
+      type: 'string',
+      autoMigrations: {
+        columnType: 'varchar'
+      }
+    },
+
+    type: {
+      type: 'string',
+      autoMigrations: {
+        columnType: 'varchar'
+      }
+    },
+
+    age: {
+      type: 'number',
+      autoMigrations: {
+        columnType: 'integer'
+      }
+    },
+
+    // obj: {
+    //   type: 'json',
+    //   autoMigrations: {
+    //     columnType: 'json'
+    //   }
+    // },
+
+    // status: {
+    //   type: 'boolean',
+    //   autoMigrations: {
+    //     columnType: 'boolean'
+    //   }
+    // },
+
+    // avatar: {
+    //   type: 'ref',
+    //   autoMigrations: {
+    //     columnType: 'binary'
+    //   }
+    // },
+
+    // Timestamps
+
+    updatedAt: {
+      type: 'number',
+      autoUpdatedAt: true,
+      autoMigrations: {
+        columnType: 'bigint'
+      }
+    },
+
+    createdAt: {
+      type: 'number',
+      autoCreatedAt: true,
+      autoMigrations: {
+        columnType: 'bigint'
+      }
     }
+
+    // first_name: 'string',
+    // last_name: 'string',
+    // email: 'string',
+    // title: 'string',
+    // phone: 'string',
+    // type: 'string',
+    // favoriteFruit: {
+    //   defaultsTo: 'blueberry',
+    //   type: 'string'
+    // },
+    // age: 'integer', // integer field that's not auto-incrementable
+    // dob: 'date',
+    // status: {
+    //   type: 'boolean',
+    //   defaultsTo: false
+    // },
+    // percent: 'float',
+    // list: 'array',
+    // obj: 'json',
   }
 
 });
