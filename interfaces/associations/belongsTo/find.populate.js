23db3c861f8071c2f44dcbf745deefc9f4511da6
Cody Stoltman <particlebanana@gmail.com>
2016-12-14 18:25:59 -0600

    update some of the belongsTo association tests

--- interfaces/associations/belongsTo/find.populate.js
+++ interfaces/associations/belongsTo/find.populate.js
@@ -2,108 +2,96 @@ var assert = require('assert');
 var _ = require('@sailshq/lodash');
 
 describe('Association Interface', function() {
-
   describe('Belongs To Association', function() {
+    describe('.find()', function() {
+      var customers;
+      var payments;
 
-    /////////////////////////////////////////////////////
-    // TEST SETUP
-    ////////////////////////////////////////////////////
+      before(function(done) {
+        var records = [
+          {
+            name: 'foo'
+          }, 
+          {
+            name: 'bar'
+          }
+        ];
 
-    var customers, payments;
-
-    before(function(done) {
-      Associations.Customer.createEach([{
-          name: 'foo'
-        }, {
-          name: 'bar'
-        }], function(err, _customers) {
-        if(err) return done(err);
-
-        // Expose results for examination below
-        customers = _customers;
-
-        Associations.Payment.createEach([{
-            amount: 1,
-            type: 'belongsTo find',
-            a_customer: customers[0].id
-          }, {
-            amount: 2,
-            type: 'belongsTo find',
-            a_customer: customers[1].id
-          }], function(err, _payments) {
-          if(err) return done(err);
+        Associations.Customer.createEach(records, function(err, _customers) {
+          if (err) {
+            return done(err);
+          }
 
           // Expose results for examination below
-          payments = _payments;
-          done();
+          customers = _customers;
+
+          var paymentRecords = [
+            {
+              amount: 1,
+              type: 'belongsTo find',
+              a_customer: customers[0].id
+            }, 
+            {
+              amount: 2,
+              type: 'belongsTo find',
+              a_customer: customers[1].id
+            }
+          ];
+
+          Associations.Payment.createEach(paymentRecords, function(err, _payments) {
+            if (err) {
+              return done(err);
+            }
+
+            // Expose results for examination below
+            payments = _payments;
+    
+            return done();
+          });
         });
       });
-    });
-
-    describe('.find()', function() {
-
-      /////////////////////////////////////////////////////
-      // TEST METHODS
-      ////////////////////////////////////////////////////
-
+    
       it('should return a customer when the populate criteria is added', function(done) {
-        Associations.Payment.find({ type: 'belongsTo find', sort: 'amount ASC' })
+        Associations.Payment.find({ 
+          type: 'belongsTo find'
+        })
+        .sort([{amount: 'ASC'}])
         .populate('a_customer')
         .exec(function(err, _payments) {
-          assert(!err, err);
+          if (err) {
+            return done(err);
+          }
 
-          assert(Array.isArray(_payments));
-          assert.strictEqual(_payments.length, 2, 'expected 2 payments, but got '+_payments.length+': '+require('util').inspect(_payments, false, null));
+          assert(_.isArray(_payments));
+          assert.strictEqual(_payments.length, 2);
 
           assert(_payments[0].a_customer);
           assert.equal(_payments[0].a_customer.id, customers[0].id);
-          assert(_payments[0].a_customer.name === 'foo',
-          'Expected `payments[0].a_customer.name`==="foo", instead payments[0].customer ===> '+ require('util').inspect(_payments[0].a_customer, false, null));
+          assert.equal(_payments[0].a_customer.name, 'foo');
 
           assert(_payments[1].a_customer);
-          assert(_payments[1].a_customer.id === customers[1].id,
-          'Expected `payments[1].a_customer.id` === '+customers[1].id+', instead payments[1].a_customer ===> '+ require('util').inspect(_payments[1].a_customer, false, null));
-          assert(_payments[1].a_customer.name === 'bar',
-          'Expected `payments[1].a_customer.name` === "bar", instead payments[1].a_customer ===> '+ require('util').inspect(_payments[1].a_customer, false, null)
-          );
+          assert.equal(_payments[1].a_customer.id, customers[1].id);
+          assert.equal(_payments[1].a_customer.name, 'bar');
+          
+          assert(_payments[0].a_customer.name);
 
-          assert(!_payments[0].toJSON().a_customer.name,
-          'Expected payments[0] to have `a_customer` populated with a `name`, but instead it looks like: '+require('util').inspect(_payments[0], false, null)
-          );
-
-          done();
+          return done();
         });
       });
 
       it('should not return a customer object when the populate is not added', function(done) {
         Associations.Payment.find()
         .exec(function(err, payments) {
-          assert.ifError(err);
+          if (err) {
+            return done(err);
+          }
 
           assert(!_.isPlainObject(payments[0].a_customer));
           assert(!_.isPlainObject(payments[1].a_customer));
 
-          done();
-        });
-      });
-
-      it('should call toJSON on associated record', function(done) {
-        Associations.Payment.find()
-        .populate('a_customer')
-        .exec(function(err, payments) {
-          assert.ifError(err);
-
-          var obj = payments[0].toJSON();
-
-          assert(!obj.type);
-          assert(obj.a_customer);
-          assert(obj.a_customer.createdAt);
-          assert(!obj.a_customer.name);
-
-          done();
+          return done();
         });
       });
     });
-
   });
 });
--- interfaces/associations/belongsTo/findOne.populate.js
+++ interfaces/associations/belongsTo/findOne.populate.js
@@ -2,79 +2,63 @@ var assert = require('assert');
 var _ = require('@sailshq/lodash');
 
 describe('Association Interface', function() {
-
   describe('Belongs To Association', function() {
+    describe('.findOne()', function() {
+      var customerRecord;
+      var paymentRecord;
 
-    /////////////////////////////////////////////////////
-    // TEST SETUP
-    ////////////////////////////////////////////////////
+      before(function(done) {
+        Associations.Customer.create({ name: 'foobar' }, function(err, customer) {
+          if (err) {
+            return done(err);
+          }
 
-    var customerRecord, paymentRecord;
+          Associations.Payment.create({ 
+            amount: 1, 
+            a_customer: customer.id 
+          })
+          .exec(function(err, payment) {
+            if (err) {
+              return done(err);
+            }
 
-    before(function(done) {
-      Associations.Customer.create({ name: 'foobar' }, function(err, customer) {
-        if(err) return done(err);
+            // Cache customer and payment
+            customerRecord = customer;
+            paymentRecord = payment;
 
-        Associations.Payment.create({ amount: 1, a_customer: customer.id }, function(err, payment) {
-          if(err) return done(err);
-
-          // Cache customer and payment
-          customerRecord = customer;
-          paymentRecord = payment;
-
-          done();
+            return done();
+          });
         });
       });
-    });
-
-    describe('.findOne()', function() {
-
-      /////////////////////////////////////////////////////
-      // TEST METHODS
-      ////////////////////////////////////////////////////
-
+    
       it('should return customer when the populate criteria is added', function(done) {
         Associations.Payment.findOne({ id: paymentRecord.id })
         .populate('a_customer')
         .exec(function(err, payment) {
-          assert.ifError(err);
+          if (err) {
+            return done(err);
+          }
 
           assert(payment.a_customer);
           assert.equal(payment.a_customer.id, customerRecord.id);
           assert.equal(payment.a_customer.name, 'foobar');
 
-          done();
+          return done();
         });
       });
 
       it('should not return a customer object when the populate is not added', function(done) {
         Associations.Payment.findOne({ id: paymentRecord.id })
         .exec(function(err, payment) {
-          assert.ifError(err);
+          if (err) {
+            return done(err);
+          }
 
           assert(!_.isPlainObject(payment.a_customer));
 
-          done();
-        });
-      });
-
-      it('should call toJSON on associated record', function(done) {
-        Associations.Payment.findOne({ id: paymentRecord.id })
-        .populate('a_customer')
-        .exec(function(err, payment) {
-          assert.ifError(err);
-
-          var obj = payment.toJSON();
-
-          assert(!obj.type);
-          assert(obj.a_customer);
-          assert(obj.a_customer.createdAt);
-          assert(!obj.a_customer.name);
-
-          done();
+          return done();
         });
       });
     });
-
   });
 });
