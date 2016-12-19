2e9dd0a4085f3999af1c903b13b5aef545b02207
Cody Stoltman <particlebanana@gmail.com>
2016-12-19 13:33:11 -0600

    update many to many populate where tests

--- interfaces/associations/manyToMany/find.populate.where.js
+++ interfaces/associations/manyToMany/find.populate.where.js
@@ -2,49 +2,57 @@ var assert = require('assert');
 var _ = require('@sailshq/lodash');
 
 describe('Association Interface', function() {
-
   describe('n:m association :: .find().populate([WHERE])', function() {
-
-    /////////////////////////////////////////////////////
-    // TEST SETUP
-    ////////////////////////////////////////////////////
-
     var driverRecord;
 
     before(function(done) {
       Associations.Driver.create({ name: 'manymany find where'}, function(err, driver) {
-        if(err) return done(err);
+        if (err) {
+          return done(err);
+        }
 
         driverRecord = driver;
 
         var taxis = [];
         for(var i=0; i<4; i++) {
-          driverRecord.taxis.add({ medallion: i });
+          taxis.push({ medallion: i });
         }
 
-        driverRecord.save(function(err) {
-          if(err) return done(err);
-          done();
+        Associations.Taxi.createEach(taxis, function(err, taxis) {
+          if (err) {
+            return done(err);
+          }
+
+          var childrenIds = _.map(taxis, function(taxi) {
+            return taxi.id;
+          });
+
+          Associations.Driver.addToCollection(driver.id, 'taxis', childrenIds)
+          .exec(function(err) {
+            if (err) {
+              return done(err);
+            }
+
+            return done();
+          });
         });
       });
     });
-
-    /////////////////////////////////////////////////////
-    // TEST METHODS
-    ////////////////////////////////////////////////////
 
     it('should return only taxis with medallions less than 2', function(done) {
       Associations.Driver.find({ name: 'manymany find where' })
       .populate('taxis', { medallion: { '<': 2 }})
       .exec(function(err, drivers) {
-        assert(!err, err);
+        if (err) {
+          return done(err);
+        }
 
-        assert(Array.isArray(drivers));
-        assert.strictEqual(drivers.length, 1);
-        assert(Array.isArray(drivers[0].taxis));
-        assert.strictEqual(drivers[0].taxis.length, 2, 'Expected first driver to have 2 taxis, but got '+drivers[0].taxis.length+', see?\n'+require('util').inspect(drivers[0]) );
+        assert(_.isArray(drivers));
+        assert.equal(drivers.length, 1);
+        assert(_.isArray(drivers[0].taxis));
+        assert.equal(drivers[0].taxis.length, 2);
 
-        done();
+        return done();
       });
     });
 
@@ -52,20 +60,21 @@ describe('Association Interface', function() {
       Associations.Driver.find({ name: 'manymany find where' })
       .populate('taxis', { skip: 1, limit: 2, sort: 'medallion ASC' })
       .exec(function(err, drivers) {
-        assert.ifError(err);
+        if (err) {
+          return done(err);
+        }
 
-        assert(Array.isArray(drivers));
-        assert.strictEqual(drivers.length, 1);
+        assert(_.isArray(drivers));
+        assert.equal(drivers.length, 1);
 
-        assert(Array.isArray(drivers[0].taxis));
+        assert(_.isArray(drivers[0].taxis));
 
-        assert.strictEqual(drivers[0].taxis.length, 2, 'Expected first driver to have 2 taxis, but got '+drivers[0].taxis.length+', see?\n'+require('util').inspect(drivers[0]));
-        assert.strictEqual(drivers[0].taxis[0].medallion, 1, 'Expected first driver\'s first taxi to have medallion===1, but heres what I got for the first driver: '+require('util').inspect(drivers[0], false, null));
-        assert.strictEqual(drivers[0].taxis[1].medallion, 2, 'Expected first driver\'s second taxi to have medallion===2, but heres what I got for the first driver: '+require('util').inspect(drivers[0], false, null));
+        assert.equal(drivers[0].taxis.length, 2);
+        assert.equal(drivers[0].taxis[0].medallion, 1);
+        assert.equal(drivers[0].taxis[1].medallion, 2);
 
-        done();
+        return done();
       });
     });
-
   });
 });
