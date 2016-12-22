b531e9c72257f6e4902b9a0cb63323ab3f5ad126
Cody Stoltman <particlebanana@gmail.com>
2016-12-22 17:31:55 -0600

    run findOrCreate tests

--- interfaces/semantic/findOrCreate.test.js
+++ interfaces/semantic/findOrCreate.test.js
@@ -2,9 +2,9 @@ var assert = require('assert');
 var _ = require('@sailshq/lodash');
 
 describe('Semantic Interface', function() {
-  describe.skip('.findOrCreate()', function() {
+  describe('.findOrCreate()', function() {
     it('should create a new record', function(done) {
-      Semantic.User.findOrCreate({ first_name: "findOrCreate()" }, { first_name: "findOrCreate()" }, function(err, user) {
+      Semantic.User.findOrCreate({ first_name: 'findOrCreate()' }, { first_name: 'findOrCreate()' }, function(err, user) {
         if (err) {
           return done(err);
         }
@@ -16,7 +16,7 @@ describe('Semantic Interface', function() {
     });
 
     it('should return a single record', function(done) {
-      Semantic.User.findOrCreate({ first_name: "findOrCreate()" }, { first_name: "findOrCreate()" }, function(err, user) {
+      Semantic.User.findOrCreate({ first_name: 'findOrCreate()' }, { first_name: 'findOrCreate()' }, function(err, user) {
         if (err) {
           return done(err);
         }
@@ -40,70 +40,16 @@ describe('Semantic Interface', function() {
     });
 
     it('should return generated timestamps', function(done) {
-      Semantic.User.findOrCreate({ first_name: "model findOrCreate()" }, { first_name: "model findOrCreate()", last_name: 'test' }, function(err, user) {
+      Semantic.User.findOrCreate({ first_name: 'model findOrCreate()' }, { first_name: 'model findOrCreate()', last_name: 'test' }, function(err, user) {
         if (err) {
           return done(err);
         }
 
         assert(user.id);
-        assert.equal(user.fullName(), 'model findOrCreate() test');
         assert(user.createdAt);
         assert(user.updatedAt);
         
         return done();
-      });
-    });
-
-    it('should take search criteria as values', function(done) {
-     Semantic.User.findOrCreate({ first_name: "findOrCreate()", last_name: 'search criteria' }, function(err, user) {
-        if (err) {
-          return done(err);
-        }
-
-        assert(user.id);
-        assert.equal(user.fullName(), 'findOrCreate() search criteria');
-        assert(user.createdAt);
-        assert(user.updatedAt);
-        
-        return done();
-      });
-    });
-
-    it('should accept array of objects', function(done) {
-     Semantic.User.findOrCreate([
-       { first_name: "findOrCreate()", last_name: 'array' },
-       { first_name: 'Mark', last_name: 'Vegetables'}
-      ], function(err, users) {
-        if (err) {
-          return done(err);
-        }
-
-        assert(users[0].id);
-        assert.equal(users[0].fullName(), 'findOrCreate() array');
-        assert(users[0].createdAt);
-        assert(users[0].updatedAt);
-
-        assert(users[1].id);
-        assert.equal(users[1].fullName(), 'Mark Vegetables');
-        assert(users[1].createdAt);
-        assert(users[1].updatedAt);
-   
-        return done();
-      });
-    });
-
-    it('should return error if search criteria length does not match values', function(done) {
-      Semantic.User.findOrCreate([
-        { first_name: "findOrCreate()" },
-        { first_name: 'Marki'}],
-      [
-        { first_name: "findOrCreate()", last_name: 'error' },
-        { first_name: 'Marki', last_name: 'Steak' },
-        { first_name: 'Max', last_name: 'Nofit' }
-      ], function(err, users) {
-        assert(err);
-        assert(!users);
-        done();
       });
     });
   });
