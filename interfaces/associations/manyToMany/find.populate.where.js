ff06d9694a630078ecac1edd0522160e323cd502
Scott Gress <scottmgress@gmail.com>
2017-01-04 08:32:15 -0600

    Add test for populating multiple many-to-many parent records

--- interfaces/associations/manyToMany/find.populate.where.js
+++ interfaces/associations/manyToMany/find.populate.where.js
@@ -3,15 +3,15 @@ var _ = require('@sailshq/lodash');
 
 describe('Association Interface', function() {
   describe('n:m association :: .find().populate([WHERE])', function() {
-    var driverRecord;
+    var driverRecords;
 
     before(function(done) {
-      Associations.Driver.create({ name: 'manymany find where'}, function(err, driver) {
+      Associations.Driver.createEach([{ name: 'manymany find where1'}, { name: 'manymany find where2'}], function(err, drivers) {
         if (err) {
           return done(err);
         }
 
-        driverRecord = driver;
+        driverRecords = drivers;
 
         var taxis = [];
         for(var i=0; i<4; i++) {
@@ -27,7 +27,7 @@ describe('Association Interface', function() {
             return taxi.id;
           });
 
-          Associations.Driver.addToCollection(driver.id, 'taxis', childrenIds)
+          Associations.Driver.addToCollection([drivers[0].id, drivers[1].id], 'taxis', childrenIds)
           .exec(function(err) {
             if (err) {
               return done(err);
@@ -40,7 +40,7 @@ describe('Association Interface', function() {
     });
 
     it('should return only taxis with medallions less than 2', function(done) {
-      Associations.Driver.find({ name: 'manymany find where' })
+      Associations.Driver.find({ name: 'manymany find where1' })
       .populate('taxis', { medallion: { '<': 2 }})
       .exec(function(err, drivers) {
         if (err) {
@@ -57,7 +57,7 @@ describe('Association Interface', function() {
     });
 
     it('should return taxis using skip and limit', function(done) {
-      Associations.Driver.find({ name: 'manymany find where' })
+      Associations.Driver.find({ name: ['manymany find where1', 'manymany find where2'] })
       .populate('taxis', { skip: 1, limit: 2, sort: 'medallion ASC' })
       .exec(function(err, drivers) {
         if (err) {
@@ -65,13 +65,19 @@ describe('Association Interface', function() {
         }
 
         assert(_.isArray(drivers));
-        assert.equal(drivers.length, 1);
+        assert.equal(drivers.length, 2);
 
         assert(_.isArray(drivers[0].taxis));
 
         assert.equal(drivers[0].taxis.length, 2);
         assert.equal(drivers[0].taxis[0].medallion, 1);
         assert.equal(drivers[0].taxis[1].medallion, 2);
+
+        assert(_.isArray(drivers[1].taxis));
+
+        assert.equal(drivers[1].taxis.length, 2);
+        assert.equal(drivers[1].taxis[0].medallion, 1);
+        assert.equal(drivers[1].taxis[1].medallion, 2);
 
         return done();
       });
