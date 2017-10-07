9e76b38adf887adf768e44f259e529d705f97d2f
Scott Gress <scottmgress@gmail.com>
2017-10-07 00:52:11 -0500

    Test "json" attribute creation against all valid JSON types
    Companion for testing https://github.com/balderdashy/sails-postgresql//commit/cae901921bdd9bf1f56e69f8afb959e704daf1de

--- interfaces/semantic/types/json.type.js
+++ interfaces/semantic/types/json.type.js
@@ -9,10 +9,10 @@ describe('Semantic Interface', function() {
           if (err) {
             return done(err);
           }
-          
+
           assert.strictEqual(createdRecord.obj, Object(createdRecord.obj));
           assert.equal(createdRecord.obj.foo, 'bar');
-          
+
           Semantic.User.findOne({id: createdRecord.id}, function (err, record) {
             if (err) {
               return done(err);
@@ -20,11 +20,116 @@ describe('Semantic Interface', function() {
 
             assert.strictEqual(record.obj, Object(record.obj));
             assert.equal(record.obj.foo, 'bar');
-            
+
             return done();
           });
         });
       });
+
+      it('should store proper array value', function(done) {
+        Semantic.User.create({ obj: ['foo','bar'] }, function(err, createdRecord) {
+          if (err) {
+            return done(err);
+          }
+
+          assert(_.isArray(createdRecord.obj));
+          assert.equal(createdRecord.obj.length, 2);
+
+          Semantic.User.findOne({id: createdRecord.id}, function (err, record) {
+            if (err) {
+              return done(err);
+            }
+
+            assert(_.isArray(record.obj));
+            assert.equal(record.obj.length, 2);
+
+            return done();
+          });
+        });
+      });
+
+      it('should store proper string value', function(done) {
+        Semantic.User.create({ obj: 'holla!' }, function(err, createdRecord) {
+          if (err) {
+            return done(err);
+          }
+
+          assert.equal(createdRecord.obj, 'holla!');
+
+          Semantic.User.findOne({id: createdRecord.id}, function (err, record) {
+            if (err) {
+              return done(err);
+            }
+
+            assert.equal(record.obj, 'holla!');
+
+            return done();
+          });
+        });
+      });
+
+      it('should store proper numeric value', function(done) {
+        Semantic.User.create({ obj: 123 }, function(err, createdRecord) {
+          if (err) {
+            return done(err);
+          }
+
+          assert.equal(createdRecord.obj, 123);
+
+          Semantic.User.findOne({id: createdRecord.id}, function (err, record) {
+            if (err) {
+              return done(err);
+            }
+
+            assert.equal(record.obj, 123);
+
+            return done();
+          });
+        });
+      });
+
+      it('should store proper boolean value', function(done) {
+        Semantic.User.create({ obj: true }, function(err, createdRecord) {
+          if (err) {
+            return done(err);
+          }
+
+          assert.equal(createdRecord.obj, true);
+
+          Semantic.User.findOne({id: createdRecord.id}, function (err, record) {
+            if (err) {
+              return done(err);
+            }
+
+            assert.equal(record.obj, true);
+
+            return done();
+          });
+        });
+      });
+
+
+      it('should store proper null value', function(done) {
+        Semantic.User.create({ obj: null }, function(err, createdRecord) {
+          if (err) {
+            return done(err);
+          }
+
+          assert.equal(createdRecord.obj, null);
+
+          Semantic.User.findOne({id: createdRecord.id}, function (err, record) {
+            if (err) {
+              return done(err);
+            }
+
+            assert.equal(record.obj, null);
+
+            return done();
+          });
+        });
+      });
+
     });
   });
+
 });
